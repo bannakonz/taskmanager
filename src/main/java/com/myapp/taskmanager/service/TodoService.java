d7ca9bf2b6cb17dbf79ae61726186ec1f28a012f
bannakonz <bannakon10@gmail.com>
2025-06-11 14:08:41 +0700

    feat(api): add api/todo/{id} for update data

--- src/main/java/com/myapp/taskmanager/service/TodoService.java
+++ src/main/java/com/myapp/taskmanager/service/TodoService.java
@@ -2,11 +2,13 @@ package com.myapp.taskmanager.service;
 
 import com.myapp.taskmanager.entity.Todo;
 import com.myapp.taskmanager.repository.TodoRepository;
+import org.apache.coyote.BadRequestException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import lombok.extern.slf4j.Slf4j;
 
 import java.util.List;
+import java.util.Optional;
 
 @Slf4j
 @Service
@@ -24,5 +26,17 @@ public class TodoService {
         log.info("Created todo: {}", todo);
 
         return todo;
+    }
+
+    public Todo updateTodo(Long id, Todo todo) throws BadRequestException {
+        Optional<Todo>  todoOptional = todoRepository.findById(id);
+        if (todoOptional.isPresent()) {
+              Todo todoGet = todoOptional.get();
+              todoGet.setTitle(todo.getTitle());
+              todoRepository.save(todoGet);
+              return todoGet;
+        } else {
+            throw new BadRequestException();
+        }
     }
 }
